ad10be3095c151c7c1846e0554153b058147767f
alexmorten <alexandrius.martinus@gmx.de>
2017-09-11 08:59:44 +0200

    #fail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'code-api.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'codeadmissiondayapi.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
@@ -83,7 +83,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: "code-api.herokuapp.com",
+  domain: "codeadmissiondayapi.herokuapp.com",
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
